d37e936c5270217aff8fa53d5d57c89ee18c8e1e
ankiyong <pop2413@gmail.com>
2021-09-29 17:43:51 +0900

    final

--- report_app/urls.py
+++ report_app/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('graphpage/',views.graphpage,name='graph')
+    path('graphpage/',views.graphpage,name='graph'),
+    path('chart/',views.chart,name='chart'),
+    path('livingchart/',views.livingchart,name='livingchart'),
+    path('safechart/',views.safechart,name='safechart'),
+    path('societychart/',views.societychart,name='societychart'),
+# /finalchart
+
     ]
--- report_app/views.py
+++ report_app/views.py
@@ -9,3 +9,19 @@ def graphpage(request):
     word = request.GET.get('chk','')
     return render(request,f'graph/{word}.html',{'word':word})
 
+
+def chart(request):
+    word = request.GET.get('chart','')
+    return render(request,'graph/population1.html')
+
+def livingchart(request):
+    word = request.GET.get('chart','')
+    return render(request,'graph/living1.html')
+
+def safechart(request):
+    word = request.GET.get('chart','')
+    return render(request,'graph/safety1.html')
+
+def societychart(request):
+    word = request.GET.get('chart','')
+    return render(request,'graph/society1.html')
